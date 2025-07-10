7eca8560ea063f35d64302f91c711311d9148d78
umasad087 <umasad087@gmail.com>
2025-07-09 20:14:38 -0400

    52th

--- infra/main.tf
+++ infra/main.tf
@@ -38,7 +38,7 @@ module "rds" {
   db_name  = var.rds_db_name
   username = var.RDS_USERNAME
   password = var.RDS_PASSWORD
-  subnet_ids             = [subnet-0f72ff5733c728657, subnet-004bd4e425f33996b]
+  subnet_ids             = ["subnet-0f72ff5733c728657", "subnet-004bd4e425f33996b"]
   vpc_security_group_ids = [module.vpc.default_security_group_id]
 
   publicly_accessible    = false
