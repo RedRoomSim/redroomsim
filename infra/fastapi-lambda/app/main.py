221b8c313170385e3fd0a100aa9266492d10a74a
umasad087 <59260232+umasad087@users.noreply.github.com>
2025-07-12 02:06:58 -0400

    Use CORSMiddleware for preflight

--- infra/fastapi-lambda/app/main.py
+++ infra/fastapi-lambda/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes.sim_router import sim_router
 from routes.logging_router import router as logging_router
@@ -11,25 +11,13 @@ origins = [
     "https://www.redroomsim.com",
 ]
 
-@app.options("/{full_path:path}")
-def preflight_handler(full_path: str):
-    return Response(
-        status_code=200,
-        headers={
-            "Access-Control-Allow-Origin": "https://redroomsim.com",
-            "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
-            "Access-Control-Allow-Headers": "Authorization, Content-Type",
-        },
-    )
-
-
-@app.middleware("http")
-async def add_cors_headers(request: Request, call_next):
-    response = await call_next(request)
-    response.headers["Access-Control-Allow-Origin"] = "https://redroomsim.com"
-    response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type"
-    return response
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(sim_router, prefix="/sim")
 app.include_router(logging_router, prefix="/logs")
