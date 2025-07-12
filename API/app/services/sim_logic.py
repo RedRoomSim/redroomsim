45ec545804e34d111dcca0347be8e37b7fc3a256
umasad087 <59260232+umasad087@users.noreply.github.com>
2025-07-12 00:11:26 -0400

    Delete API/app directory

--- API/app/services/sim_logic.py
+++
@@ -1,9 +0,0 @@
-import json
-from pathlib import Path
-from models.simmodels import SimScenario
-
-def load_scenario_from_file(file_name: str) -> SimScenario:
-    file_path = Path(__file__).resolve().parent.parent / "data" / file_name
-    with open(file_path) as f:
-        data = json.load(f)
-    return SimScenario(**data)
