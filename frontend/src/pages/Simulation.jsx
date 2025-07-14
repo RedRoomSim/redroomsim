c1c45581545bb11753a795b908fd94e82d8b8dec
umasad087 <umasad087@gmail.com>
2025-07-14 01:57:18 -0400

    150th

--- frontend/src/pages/Simulation.jsx
+++ frontend/src/pages/Simulation.jsx
@@ -241,7 +241,7 @@ const Simulation = () => {
                     </div>
                   </>
                 )}
-                <div className="flex justify-end mt-4">
+                <div className="justify-top mt-4">
                   <button
                     onClick={handleEndSimulation}
                     className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
