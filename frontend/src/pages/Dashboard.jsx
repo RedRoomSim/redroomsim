801164ca92cb8759a7a1f6d415143a74891344f2
umasad087 <umasad087@gmail.com>
2025-07-23 11:20:25 -0400

    Progress dashboard cleanup

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -26,7 +26,9 @@ import ProgressTracker from "../components/ProgressDashboard/ProgressTracker";
 const Dashboard = () => {
   return (
     <div>
+      {/*
       <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+      */}
       <ProgressTracker />
     </div>
   );
